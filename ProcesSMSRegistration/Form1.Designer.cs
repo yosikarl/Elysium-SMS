b094e5a95be3ee997347a53a8f34a8da1e58b03e
Yosi Karl YoSigal <yosi@yosigal.com>
2017-03-04 11:57:09 +0200

    Initial Commit (Feb 11th, 2017)

---
+++ ProcesSMSRegistration/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace ProcesSMSRegistration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRecipients = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(578, 218);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(578, 122);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "SendSMS";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(11, 32);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(642, 22);
+            this.txtMessage.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Message:";
+            // 
+            // txtRecipients
+            // 
+            this.txtRecipients.Location = new System.Drawing.Point(11, 89);
+            this.txtRecipients.Name = "txtRecipients";
+            this.txtRecipients.Size = new System.Drawing.Size(275, 22);
+            this.txtRecipients.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Recipients:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 253);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRecipients);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRecipients;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ProcesSMSRegistration/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Microsoft.VisualBasic.FileIO;
+
+namespace ProcesSMSRegistration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // MessageBox.Show("Hello, World!");
+
+            openFileDialog1.Filter = "CSV files|*.csv";
+            openFileDialog1.ShowDialog();
+            string fileName = openFileDialog1.FileName;
+
+            MessageBox.Show(fileName);
+
+            saveFileDialog1.FileName = "SMSReceipients.csv";
+            saveFileDialog1.DefaultExt = ".csv";
+            saveFileDialog1.AddExtension = true;
+            saveFileDialog1.InitialDirectory = openFileDialog1.InitialDirectory;
+            saveFileDialog1.ShowDialog();
+            string OutPutFileName = saveFileDialog1.FileName;
+
+            MessageBox.Show(OutPutFileName);
+
+            using (TextFieldParser parser = new TextFieldParser(fileName))
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(OutPutFileName,false,Encoding.UTF8))
+                {
+                    file.WriteLine("שם פרטי, טלפון סלולרי, שיוך לקבוצה");
+
+                    parser.TextFieldType = FieldType.Delimited;
+                    parser.SetDelimiters(",");
+
+                    //skip 1st line
+                    parser.ReadFields();
+
+                    while (!parser.EndOfData)
+                    {
+                        //Processing row
+                        string[] fields = parser.ReadFields();
+                        string groupNum = fields[2];
+                        string Name1 = fields[3];
+                        string phone1 = fields[4];
+                        string Name2 = fields[5];
+                        string phone2 = fields[6];
+                        file.WriteLine(Name1 + "," + phone1 + "," + groupNum);
+                        file.WriteLine(Name2 + "," + phone2 + "," + groupNum);
+                    }
+                }
+
+            }
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private const string infoForYouUserName = "frc";
+        private const string infoForYouPWD = "elysium1937";
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WS_SendSMS.SendMessageSoapClient SendSMS = new WS_SendSMS.SendMessageSoapClient();
+            String Result = SendSMS.SendSmsDetailed(infoForYouUserName, infoForYouPWD,
+                                                    txtMessage.Text,
+                                                    "","", "","",
+                                                    txtRecipients.Text,
+                                                    "","",0,
+                                                    new DateTime(1900, 01, 01, 00, 00, 00),
+                                                    "Elysium1937",
+                                                    "0000", 0);
+            MessageBox.Show(Result);
+        }
+    }
+}
